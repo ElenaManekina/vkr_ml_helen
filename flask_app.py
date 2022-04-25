f9e10247965e5a2110d319e8b5914463eedf3b11
ehidina <elena.manekina@gmail.com>
2022-04-26 00:23:06 +0300

    flask_app unacceptable parameter value handle

--- flask_app.py
+++ flask_app.py
@@ -13,19 +13,31 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
+        parameters = {}
+
+        for key, value in request.form.items():
+            try:
+                parameter_value = float(value)
+                if parameter_value < 0.0 or parameter_value > 1.0:
+                    raise ValueError('Out of range!')
+                parameters[key] = parameter_value
+
+            except ValueError:
+                return render_template('parameter_error.html', title=key, value=value)
+
         predicted_value = predict_matrix([[
-            float(request.form['density_kg/m3']),
-            float(request.form['elasticity_modulus_GPa']),
-            float(request.form['hardener_quantity_m%']),
-            float(request.form['epoxy_group_amount_%']),
-            float(request.form['flash_T_С_2']),
-            float(request.form['surface_density_g/m2']),
-            float(request.form['mod_of_elast_under_tension_GPa']),
-            float(request.form['strength_under_tension_MPa']),
-            float(request.form['resin_consumption_g/m2']),
-            float(request.form['patch_angle_degree']),
-            float(request.form['patch_step']),
-            float(request.form['patch_density'])
+            parameters['density_kg/m3'],
+            parameters['elasticity_modulus_GPa'],
+            parameters['hardener_quantity_m%'],
+            parameters['epoxy_group_amount_%'],
+            parameters['flash_T_С_2'],
+            parameters['surface_density_g/m2'],
+            parameters['mod_of_elast_under_tension_GPa'],
+            parameters['strength_under_tension_MPa'],
+            parameters['resin_consumption_g/m2'],
+            parameters['patch_angle_degree'],
+            parameters['patch_step'],
+            parameters['patch_density']
         ]])
         return render_template('predict_result.html', result=predicted_value[0][0])
 
@@ -34,15 +46,15 @@ def predict():
         if 'Соотношение матрица-наполнитель' in parameter_names:
             parameter_names.pop('Соотношение матрица-наполнитель')
 
-        parameters = []
+        template_data = []
 
         for key, value in parameter_names.items():
-            parameters.append({
+            template_data.append({
                 'id': value,
                 'title': key,
                 'default_value': 0.0,
             })
-        return render_template('predict_form.html', parameters=parameters)
+        return render_template('predict_form.html', parameters=template_data)
 
 
 if __name__ == '__main__':
